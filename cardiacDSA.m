fcebabe96865efa611f41d09516365065c372fc3
Andrew Davies <a.g.davies@leeds.ac.uk>
2020-04-16 17:20:35 +0100

    fixed incorrect argument spelling

--- cardiacDSA.m
+++ cardiacDSA.m
@@ -3,4 +3,4 @@ if nargin < 2
     frameRate  = 15; % default is 15 frames per second
 end
 endPhaseTimes = detectCardiacPhase(img, frameRate);
-dsa = calculateDSA(img, framerate, endPhaseTimes);
+dsa = calculateDSA(img, frameRate, endPhaseTimes);
